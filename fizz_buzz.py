6f903ba529c75d70e1c94feb4297ae645afb34dd
Josep Galbis sebastian <josephcode@MacBook-Pro-de-Josep.local>
2024-10-02 23:17:28 +0200

    Mini Reto fizz buzz con Phyton

---
+++ fizz_buzz.py
@@ -0,0 +1,16 @@
+"""
+EL FAMOSO "FIZZ BUZZ"
+Escribe un programa que muestre por consola (con un print) los números de 1 al 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+Múltiplos de 3 por la palabra "fizz"
+Múltiplos de 5 por la palabra "buzz"
+Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz"
+"""
+
+for number in range(1, 101):
+    output = ""
+    if number % 3 == 0:
+        output += "fizz"
+    if number % 5 == 0:
+        output += "buzz"
+    
+    print(output or number)
