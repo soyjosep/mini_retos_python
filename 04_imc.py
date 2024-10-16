3e731bbac42c9d8ed4da4e3bb3351d5c461a3ceb
Joseph Code <pepet_21@hotmail.com>
2024-10-16 17:05:57 +0200

    ejercicio 04 v0.0.2

--- 04_imc.py
+++ 04_imc.py
@@ -12,10 +12,10 @@ print(f"IMC: {imc: .2f}")
 
 if imc < 18.5:
     print("Peso bajo")
-elif 18.5 <= imc > 24.9:
+elif 18.5 <= imc < 24.9:
     print("Peso normal")
 
-elif 25 <= imc > 29.9:
+elif 25 <= imc < 29.9:
     print("Sobrepeso")
 else:
     print("Obesidad")
