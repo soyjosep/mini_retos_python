8eb2becb735db352d7f0a91694c21a7cfb8a1bb6
Joseph Code <pepet_21@hotmail.com>
2024-10-29 02:03:16 +0100

    mini reto 10 v1.0.0

--- 10_lista_pokemon.py
+++ 10_lista_pokemon.py
@@ -5,16 +5,33 @@ utilizando la PokéAPI.
 """
 
 import requests
-def listar_pokemons(limite=151):
-    url = f"https://pokeapi.co/api/v2/pokemon?limit={limite}"
-    respuesta = requests.get(url)
 
-    if respuesta.status_code == 200:
-        datos = respuesta.json()
-        for index, pokemon in enumerate(datos["results"], start=1):
-            print(f"{index}. {pokemon['name'].capitalize()}")
+def obtener_pokemon(limit=151):
+    """ 
+    Obtiene una lista de nombres de los primeros Pokémon utilizando la PokeAPI.
+
+    Parámetros:
+    limit (int): El número de Pokémon a obtener.
+
+    Retorna:
+    list: Una lista con los nombres de los Pokémon.
+    """
+    url = f"https://pokeapi.co/api/v2/pokemon?limit={limit}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        nombres_pokemon = [pokemon['name'] for pokemon in data['results']]
+        return nombres_pokemon
     else:
-        print("No se pudo obtener la lista de pokémon.")
+        print("Error al conectar con la PokéAPI.")
+        return[]
+    
+def main():
+        print("Lista de los primeros 151 Pokémon:\n")
+        nombres_pokemon = obtener_pokemon()
+        for idx, nombre in enumerate(nombres_pokemon, start=1):
+             print(f"{idx}. {nombre.capitalize()}")
 
-# Ejemplo de uso
-listar_pokemons()
+if __name__ == "__main__":
+    main()
