fe3c21454033b0d129b1505152946151e5953586
Joseph Code <pepet_21@hotmail.com>
2024-10-30 23:33:29 +0100

    mini reto 11 v 0.1.0

--- 11_media.py
+++ 11_media.py
@@ -4,14 +4,13 @@ Calcula la media de un
 listado de números.
 """
 
-kim_yeji = [9.8, 10.4, 10.5, 9.3, 9.7, 10.7, 10.4, 9.9, 10.4, 10.4, 10.0, 10.3, 9.9, 10.2, 9.8, 10.1, 10.2, 9.7, 10.2, 10.0, 9.4, 10.5, 9.7, 9.8]
-yusuf_dikec = [9.0, 10.3, 10.2, 10.6, 10.1, 10.6, 9.9, 10.5, 9.2, 9.5, 10.8, 10.7, 10.4, 10.6, 9.1]
+def calcular_media(numeros):
+    if not numeros:
+        return "La lista esta vacia, no se puede calcular la media"
+    return sum(numeros) / len(numeros)
 
-def mean(numbers):
-    return sum(numbers) / len(numbers)
-
-kim_mean = mean(kim_yeji)
-yusuf_mean = mean(yusuf_dikec)
-
-print(f"Media Kim {kim_mean: .1f}")
-print(f"Media Yusuf {yusuf_mean: .1f}")
+#Ejemplo de uso
+numeros = [10, 20, 30, 40, 50]
+media = calcular_media(numeros)
+print(f"La media de los numeros es: {media}")
+    
