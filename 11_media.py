e4b242f6757079d0b46f7893c7ab8f933f0cc9a2
Joseph Code <pepet_21@hotmail.com>
2024-10-30 23:52:06 +0100

    mini reto 11 v1.0.0

--- 11_media.py
+++ 11_media.py
@@ -5,12 +5,38 @@ listado de números.
 """
 
 def calcular_media(numeros):
+    """ 
+    Calcoula la media de una lista de números.
+    
+    Parámetros:
+    numeros (list): Lista de números (float o int) para calcular la media.
+
+    Retorna:
+    float: La media de los números en la lista.
+    """
+
     if not numeros:
-        return "La lista esta vacia, no se puede calcular la media"
+        return 0
     return sum(numeros) / len(numeros)
 
-#Ejemplo de uso
-numeros = [10, 20, 30, 40, 50]
-media = calcular_media(numeros)
-print(f"La media de los numeros es: {media}")
+def main():
+    print("Cálculo de la Media de un Listado de Números")
+    numeros = []
+    while True:
+        entrada = input("Ingrese un número (o 'fin' para calcular la media): ")
+        if entrada.lower() == 'fin':
+            break
+        try:
+            numero = float(entrada)
+            numeros.append(numero)
+        except ValueError:
+            print("Entrada no válida. Por favor, ingrese un número.")
+    if numeros:
+        media = calcular_media(numeros)
+        print(f"La media de los números ingresados es: {media: .2f}")
+    else:
+        print("No se ingresaro números para calcular la media.")
+
+if __name__ == "__main__":
+    main()
     
